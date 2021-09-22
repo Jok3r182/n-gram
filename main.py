dd49d0a3d74dc581529df653a356f849a220a0e9
Jok3r182 <71766496+Jok3r182@users.noreply.github.com>
2021-09-22 17:43:07 +0300

    mini bug fixes, refactoring!

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-import collections
 import random
 
-# i: bus n = 2 atveju nuo 0 iki galo ir nuo 1 elemento iki galo
 import re
 
 
+# i: bus n = 2 atveju nuo 0 iki galo ir nuo 1 elemento iki galo
 def ngrams(sequence, n):
     # is sequence gauname du sakinius jei n = 2, jei 3 dasises sakinys nuo is: bus machine learning is not boring it
     # is   fun learning is     not boring it is fun su zip siuos sakinius apjungiame sequence turimus machine
@@ -14,41 +13,57 @@ def ngrams(sequence, n):
     # feeds them as separate arguments into the function.
 
 
-f = open("text.txt", "r")
+def read_text_from_file():
+    file = open("salman_says.txt", "r")
+    return re.sub(r'[^\w\s]', '', file.read()).lower()
+    # paliekame tik tarpus ir zodzius regex pagalba, regex pagalba yra replacinami '', s tarpas, w zodis
 
-sentence = re.sub(r'[^\w\s]', '',
-                  f.read()).lower()  # paliekame tik tarpus ir zodzius regex pagalba, regex pagalba yra replacinami '', s tarpas, w zodis
 
-n = 2
-ngrams = ngrams(sentence.split(), n)
-wordToPredict = ''
-wordHistory = ''
-wordMap = dict()
-ngramPhrases = dict()
-# fix paima learning is o is neturi sekancio
-for count, grams in enumerate(ngrams):
-    ngramPhrases[count] = ' '.join(grams)  # apjungiame grams i frazes
-    for item in grams:
-        if item != grams[-1]:
-            wordHistory = wordHistory + item + ' '  # prieje kiekviena gram saugomes paskutini zodi ir pries tai
-            # buvusius
+def map_words(n_grams):
+    word_to_predict = ''
+    word_history = ''
+    word_map = dict()
+    ngram_phrases = dict()
+    # fix paima learning is o is neturi sekancio
+    for count, grams in enumerate(n_grams):
+        ngram_phrases[count] = ' '.join(grams)  # apjungiame grams i frazes
+        for item in grams:
+            if item != grams[-1]:
+                word_history = word_history + item + ' '  # prieje kiekviena gram saugomes paskutini zodi ir pries tai
+                # buvusius
+            else:
+                word_to_predict = item
+        word_history = word_history.strip()
+        word_to_predict = word_to_predict.strip()
+        word_map.setdefault(word_history, []).append(
+            word_to_predict)  # sukuriame map su unique key, kuris tures istorija kiekvienos istorijos galimus variantus
+        word_history = ''
+        word_to_predict = ''
+    return word_map, ngram_phrases
+
+
+def generate_text(ngram_map, n_grams_phrases, word_number):
+    gen_sentence = str(random.choice(list(n_grams_phrases.values()))).split()  # imame 1 random is turimu ngram
+    # print(str(gen_sentence))
+    for i in range(word_number - n):
+        if ngram_map.get(' '.join(gen_sentence[-(n - 1):])) is not None:
+            # jei toks paskutinis yra kieno nors istorija, - : reiskia jog eisime nuo galo kazkoki skaiciu -2:
+            # reiskia 2 paskutniai zodziai
+            gen_sentence.append(random.choice(ngram_map.get(' '.join(gen_sentence[-(n - 1):]))))
+            # jungiame prie sakinio
         else:
-            wordToPredict = item
-    wordHistory = wordHistory.strip()
-    wordToPredict = wordToPredict.strip()
-    wordMap.setdefault(wordHistory, []).append(
-        wordToPredict)  # sukuriame map su unique key, kuris tures istorija kiekvienos istorijos galimus variantus
-    wordHistory = ''
-    wordToPredict = ''
+            # tam, kad sugeneruoti lygiai nurodyta skaiciu zodziu, nes bus i = 1, bet fraze jei n = 3
+            # prastumiame numeracija per tiek zodziu kiek sugeneravome, nes jau 1 prasukta
+            word_number = word_number - n - 1
+            gen_sentence = gen_sentence + str(random.choice(list(n_grams_phrases.values()))).split()
+            # jei ne generuojame toliau, tokio dydzio koks ngram kad random per daug nebutu
+    print(' '.join(gen_sentence))
 
-# print(wordMap)
 
-genSentence = str(random.choice(list(ngramPhrases.values()))).split()  # imame 1 random is turimu ngram
-# print(str(genSentence))
-for i in range(50):
-    if wordMap.get(' '.join(genSentence[-(
-            n - 1):])) is not None:  # jei toks paskutinis yra kieno nors istorija, - : reiskia jog eisime nuo galo kazkoki skaiciu -2: reiskia 2 paskutniai zodziai
-        genSentence.append(random.choice(wordMap.get(' '.join(genSentence[-(n - 1):]))))  # jungiame prie sakinio
-    else:
-        genSentence.append(random.choice(list(ngramPhrases.values())).split()[0])  # jei ne generuojame toliau
-print(' '.join(genSentence))
+# tam, kad ikelus sita package kitur nebutu paleisti visi metodai
+if __name__ == "__main__":
+    n = 2
+    ngrams = ngrams(read_text_from_file().split(), n)
+    word_map, ngram_phrases = map_words(ngrams)
+    #print(word_map)
+    generate_text(word_map, ngram_phrases, 50)
