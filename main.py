c7f3421fb4153eb7e5d79984007357d8756096ff
Jok3r182 <71766496+Jok3r182@users.noreply.github.com>
2021-09-22 13:37:55 +0300

    fix!

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import collections
 import random
 
-
 # i: bus n = 2 atveju nuo 0 iki galo ir nuo 1 elemento iki galo
+import re
+
+
 def ngrams(sequence, n):
     # is sequence gauname du sakinius jei n = 2, jei 3 dasises sakinys nuo is: bus machine learning is not boring it
     # is   fun learning is     not boring it is fun su zip siuos sakinius apjungiame sequence turimus machine
@@ -12,21 +14,10 @@ def ngrams(sequence, n):
     # feeds them as separate arguments into the function.
 
 
-sentence = "Once upon a time, a very long time ago now, about last Friday, Winnie-the-Pooh lived in a forest all by " \
-           "himself under the name of Sanders. ('What does 'under the name' mean?' asked Christopher Robin. 'It means " \
-           "he had the name over the door in gold letters, and lived under it.' 'Winnie-the-Pooh wasnt quite sure," \
-           "' said Christopher Robin. 'Now I am,' said a growly voice. 'Then I will go on,' said I.) One day when he " \
-           "was out walking, he came to an open place in the middle of the forest, and in the middle of this place " \
-           "was a large oak-tree, and, from the top of the tree, there came a loud buzzing-noise. Winnie-the-Pooh sat " \
-           "down at the foot of the tree, put his head between his paws and began to think. First of all he said to " \
-           "himself: 'That buzzing-noise means something. You dont get a buzzing-noise like that, just buzzing and " \
-           "buzzing, without its meaning something. If there's a buzzing-noise, somebodys making a buzzing-noise, " \
-           "and the only reason for making a buzzing-noise that I know of is because youre a bee.' Then he thought " \
-           "another long time, and said: 'And the only reason for being a bee that I know of is making honey.' And " \
-           "then he got up, and said: 'And the only reason for making honey is so as I can eat it.' So he began to " \
-           "climb the tree".replace(',', ' ').replace("'", ' ').replace('.', ' ').replace(
-    ')', ' ').replace('(', ' ') \
-    .lower()
+f = open("text.txt", "r")
+
+sentence = re.sub(r'[^\w\s]', '',
+                  f.read()).lower()  # paliekame tik tarpus ir zodzius regex pagalba, regex pagalba yra replacinami '', s tarpas, w zodis
 
 n = 2
 ngrams = ngrams(sentence.split(), n)
@@ -44,6 +35,7 @@ for count, grams in enumerate(ngrams):
         else:
             wordToPredict = item
     wordHistory = wordHistory.strip()
+    wordToPredict = wordToPredict.strip()
     wordMap.setdefault(wordHistory, []).append(
         wordToPredict)  # sukuriame map su unique key, kuris tures istorija kiekvienos istorijos galimus variantus
     wordHistory = ''
@@ -51,11 +43,12 @@ for count, grams in enumerate(ngrams):
 
 # print(wordMap)
 
-genSentence = str(random.choice(list(ngramPhrases.values()))).split(' ')  # imame 1 random is turimu ngram
+genSentence = str(random.choice(list(ngramPhrases.values()))).split()  # imame 1 random is turimu ngram
+# print(str(genSentence))
 for i in range(50):
-    if wordMap.get(genSentence[-1]) is not None:  # jei toks paskutinis yra kieno nors istorija
-        genSentence.append(random.choice(wordMap[genSentence[-1]]))  # jungiame prie sakinio
+    if wordMap.get(' '.join(genSentence[-(
+            n - 1):])) is not None:  # jei toks paskutinis yra kieno nors istorija, - : reiskia jog eisime nuo galo kazkoki skaiciu -2: reiskia 2 paskutniai zodziai
+        genSentence.append(random.choice(wordMap.get(' '.join(genSentence[-(n - 1):]))))  # jungiame prie sakinio
     else:
-        genSentence = genSentence + str(random.choice(list(ngramPhrases.values()))).split(
-            ' ')  # jei ne generuojame toliau
+        genSentence.append(random.choice(list(ngramPhrases.values())).split()[0])  # jei ne generuojame toliau
 print(' '.join(genSentence))
